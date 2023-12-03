51551195fb6bbff991b8f278167ca15be0389f25
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-03 23:26:40 +0530

    update

---
+++ src/components/Section.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+type sectionProps = {
+    title?: string,
+    children: ReactNode
+}
+
+const Section = () => {
+  return (
+    <div>Section</div>
+  )
+}
+
+export default Section
